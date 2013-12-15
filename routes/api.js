ed0a1ab9a88ddb68436539c2767efdf272f6c727
Nikodem Jaworski <nikodemjaworski@gmail.com>
2013-12-15 17:32:45 +0100

    Timetable parser tested and implemented

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
-    var cheerio = require('cheerio');
     var request = require('../lib/request');
+    var parser = require('../lib/parser');
+    var DAY = 86400000;
     
     
     // Get element list by type
@@ -10,23 +11,56 @@ module.exports = function (app) {
     
     
     // GET /weeks
-    // Return an array with available weeks numbers
+    // Returns an array with available weeks numbers
     app.get("/weeks", function (req, res) {
-        var weeks = [];
         
-        request('http://stupid.gso-koeln.de/frames/navbar.htm', 500, function (body) {
-            $ = cheerio.load(body);
+        request('http://stupid.gso-koeln.de/frames/navbar.htm', DAY, function (body) {
             
-            $('select[name=week] > option').each(function(i, elem) {
-              weeks.push(elem.attribs.value);
+            parser.weeks.parse(body, function(weeks){
+                res.json(weeks);
             });
             
-            res.json(weeks);
         });
+        
     });
     
-    // Get element list by type
+    // GET /elements/:type
+    // Returns an array with all available elements for given type
     app.get("/elements/:type(teachers|classes|rooms)", function (req, res) {
-        res.json(['yes']);
+        
+        request('http://stupid.gso-koeln.de/frames/navbar.htm', DAY, function (body) {
+            
+            parser.elements.parse(body, req.params.type, function(elements){
+                res.json(elements);
+            });
+            
+        });
+        
+    });
+    
+    // GET /timetable/:type/:element
+    // Returns an array with all available elements for given type
+    app.get("/timetable/:type/:element/:week", function (req, res) {
+        
+        request('http://stupid.gso-koeln.de/frames/navbar.htm', DAY, function (body) {
+            
+            parser.elements.getRemoteId(body, req.params.type, req.params.element, function(remoteId){
+                
+                var typesMap = {
+                    teachers: 't',
+                    classes: 'c',
+                    rooms: 'r'
+                };
+                
+                request('http://stupid.gso-koeln.de/' + req.params.week + '/' + typesMap[req.params.type] + '/' + typesMap[req.params.type] + remoteId + '.htm', DAY, function(body){
+                    parser.timetable.parse(body, function(result){
+                       res.send(result); 
+                    });
+                });
+            
+            });
+            
+        });
+        
     });
 };
