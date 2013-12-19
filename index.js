28504cb22c2165d70756859ce1bb06f5bd256a61
Nikodem Jaworski <nikodemjaworski@gmail.com>
2013-12-19 12:22:38 +0100

    Added max-age header to static content. Enabled gzip.

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ var engines = require('consolidate');
 
 var app = express();
 
+// View Config
 app.engine('html', engines.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.set('view cache', false);
 
+// Cache and Compress
+var month = 60*60*24*30*24*30;
+app.use(express.compress());
+app.use(express.static('public', { maxAge: month }));
+
+
+//Logger
 app.use(express.logger('dev'));
-app.use(express.static('public'))
+
+// Load Routes
 require('./routes')(app);
 
-app.listen(3001, 'localhost');
+app.listen(3001);
